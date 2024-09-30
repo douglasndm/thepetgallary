a85d5fc1180fb6f265a0ed81c22a8260d37e9ca4
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-09-30 18:32:47 -0300

    fix: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
-    root: true,
-    extends: [
-        '@react-native',
-        '@rocketseat/eslint-config/react'
-    ],
-    plugins: [
-        'react-hooks',
-        'prettier'
-    ],
-    rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        'prettier/prettier': 'error',
-    },
+	root: true,
+	extends: ['@react-native', 'prettier'],
+	plugins: ['react-hooks', 'prettier'],
+	rules: {
+		'react-hooks/rules-of-hooks': 'off',
+		'prettier/prettier': 'error',
+	},
 };
