897698e8210127af13c6e4693b1c42bfce368b64
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-10-01 02:14:26 -0300

    Adding animation on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import LottieView from 'lottie-react-native';
 
-import { Container, TextContainer, AppTitle, PageTitle } from './styles';
+import catLogo from '@animations/cat_logo.lottie';
+
+import { Container, Content, TextContainer, AppTitle } from './styles';
 
 interface Props {
 	currentPageTitle: string;
 }
 
-const header: React.FC<Props> = ({ currentPageTitle }: Props) => {
+const header: React.FC<Props> = ({}: Props) => {
+	const animRef = useRef<LottieView>(null);
+
 	return (
 		<Container>
-			<TextContainer>
-				<AppTitle>The Pet Gallery</AppTitle>
-				<PageTitle>{currentPageTitle}</PageTitle>
-			</TextContainer>
+			<Content onPress={() => animRef.current?.play()}>
+				<TextContainer>
+					<AppTitle>The Pet Gallery</AppTitle>
+				</TextContainer>
+
+				<LottieView
+					ref={animRef}
+					source={catLogo}
+					style={{
+						width: 100,
+						height: 100,
+						marginLeft: -25,
+					}}
+					autoPlay
+					loop={false}
+				/>
+			</Content>
 		</Container>
 	);
 };
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,22 +1,19 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
-    align-items: center;
+export const Container = styled.SafeAreaView``;
+
+export const Content = styled.Pressable`
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
 `;
 
 export const TextContainer = styled.View`
-    padding: 20px 0;
+	padding: 20px 0;
 `;
 
 export const AppTitle = styled.Text`
-    font-size: 18px;
-    text-align: center;
-    color: #A8A8A8;
-`;
-
-export const PageTitle = styled.Text`
-    font-size: 22px;
-    font-weight: bold;
-    text-align: center;
-    color: #383838;
+	font-size: 18px;
+	text-align: center;
+	color: #737573;
 `;
