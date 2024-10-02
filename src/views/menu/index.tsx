e209a662d59c5c3e92981c6b8ac999dbf56f2894
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-10-02 19:39:12 -0300

    Adding about

--- src/views/menu/index.tsx
+++ src/views/menu/index.tsx
@@ -1,10 +1,154 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Linking } from 'react-native';
+import LottieView from 'lottie-react-native';
 
-// import { Container } from './styles';
+import dogDancing from '@animations/dog_dancing.lottie';
+
+import Header from '@components/header';
+import Padding from '@components/padding';
+
+import {
+	Container,
+	Content,
+	Title,
+	AppName,
+	Info,
+	AttibuitionContainer,
+	Attibution,
+	AttibutionLink,
+} from './styles';
 
 const Menu: React.FC = () => {
-	return <View />;
+	return (
+		<Container>
+			<Header />
+			<Title>Sobre</Title>
+			<Content>
+				<AppName>The Pet Gallery</AppName>
+				<Info>Sua dose diária de lambeijo</Info>
+
+				<Info>Desenvolvido por Douglas Nunes de Mattos</Info>
+
+				<LottieView
+					source={dogDancing}
+					style={{
+						width: 250,
+						height: 250,
+						marginTop: 30,
+						alignSelf: 'center',
+					}}
+					autoPlay
+					loop
+				/>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Mais aplicativos{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL('https://douglasndm.dev')
+							}
+						>
+							aqui
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Logo de pikisuperstar no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://br.freepik.com/vetores-gratis/pessoas-de-ilustracao-plana-com-animais-de-estimacao_15292679.htm'
+								)
+							}
+						>
+							Freepik
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Animação de carregamento de doguinhos de Syed Asim Ali
+						Shah no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://lottiefiles.com/free-animation/dog-walking-7K4JCxjvHm'
+								)
+							}
+						>
+							LottieFiles
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Animação de carregamento de gatinhos de Evgeny Marinin
+						no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://lottiefiles.com/free-animation/cat-loader-F4ZybShCAh'
+								)
+							}
+						>
+							LottieFiles
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Animação do Header por Diane Picchiottino no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://lottiefiles.com/free-animation/loader-cat-dWUie0iIVk'
+								)
+							}
+						>
+							LottieFiles
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Animação no sobre por Eugene Croquette no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://lottiefiles.com/free-animation/dancing-dog-2x20-cSm026rhBF'
+								)
+							}
+						>
+							LottieFiles
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+
+				<AttibuitionContainer>
+					<Attibution>
+						Animação de falta de conexão de Hishara Dilshan HishD no{' '}
+						<AttibutionLink
+							onPress={() =>
+								Linking.openURL(
+									'https://lottiefiles.com/free-animation/no-connection-zD7pwxybpR'
+								)
+							}
+						>
+							LottieFiles
+						</AttibutionLink>
+					</Attibution>
+				</AttibuitionContainer>
+			</Content>
+			<Padding />
+		</Container>
+	);
 };
 
 export default Menu;
--- src/views/menu/styles.ts
+++ src/views/menu/styles.ts
@@ -1,5 +1,41 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-	flex: 1;
+export const Container = styled.ScrollView`
+	background-color: #fff;
+`;
+
+export const Content = styled.View`
+	background-color: #f8cdb9;
+
+	align-self: center;
+	width: 90%;
+	border-radius: 12px;
+
+	padding: 20px;
+`;
+
+export const AppName = styled.Text`
+	font-size: 22px;
+	font-weight: bold;
+	margin-bottom: 5px;
+`;
+export const Info = styled.Text`
+	font-size: 16px;
+`;
+
+export const Title = styled.Text`
+	margin-left: 25px;
+	margin-bottom: 10px;
+	font-size: 17px;
+`;
+
+export const AttibuitionContainer = styled.View`
+	margin-top: 15px;
+`;
+
+export const Attibution = styled.Text``;
+
+export const AttibutionLink = styled.Text`
+	color: #000;
+	text-decoration-line: underline;
 `;
