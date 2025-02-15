82b475d2c2a32275d728361b8319d15957c50b3d
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-15 16:14:35 -0300

    Adding a view to add vaccines

---
+++ src/views/vaccines/add/index.tsx
@@ -0,0 +1,224 @@
+import React, { useState, useCallback } from 'react';
+import { Modal, View } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { showMessage } from 'react-native-flash-message';
+import DatePicker from 'react-native-ui-datepicker';
+
+import { captureException } from '@services/exceptionsHandler';
+
+import { formatDate } from '@utils/data';
+import { saveVaccine } from '@utils/vaccines/save';
+
+import Header from '@components/header';
+import Button from '@components/button';
+import Loading from '@components/loading';
+
+import {
+	Container,
+	Content,
+	Input,
+	DateContainer,
+	DateTextContent,
+	DateText,
+	DateRemoveButton,
+	DateRemoveIcon,
+	Label,
+	CloseButton,
+	Icon,
+} from './styles';
+
+const VaccinesAdd: React.FC = () => {
+	const { pop } = useNavigation<NativeStackNavigationProp<AppRoutes>>();
+	const { params } = useRoute<RouteProp<AppRoutes, 'VaccinesAdd'>>();
+
+	const [isLoading, setIsLoading] = useState(false);
+	const [isModalAdministeredDateVisible, setIsModalAdministeredDateVisible] =
+		useState(false);
+	const [isModalNextDateVisible, setIsModalNextDateVisible] = useState(false);
+
+	const [showUseDate, setShowUseDate] = useState(false);
+	const [showUseNextDate, setShowUseNextDate] = useState(false);
+
+	const [name, setName] = useState('');
+	const [administeredDate, setAdministeredDate] = useState<Date>(new Date());
+	const [nextDoseDate, setNextDoseDate] = useState<Date>(new Date());
+	const [notes, setNotes] = useState('');
+
+	const handleSave = useCallback(async () => {
+		try {
+			if (name.trim() === '') {
+				return showMessage({
+					message: 'Por favor, informe o nome da vacina.',
+					type: 'warning',
+				});
+			}
+
+			setIsLoading(true);
+
+			await saveVaccine({
+				petId: params.petId,
+				vaccine: {
+					name: name.trim(),
+					date_administered: showUseDate ? administeredDate : null,
+					next_dose_date: showUseNextDate ? nextDoseDate : null,
+					notes: notes.trim(),
+				},
+			});
+
+			showMessage({
+				message: 'Vacina cadastrada com sucesso.',
+				type: 'success',
+			});
+
+			pop();
+		} catch (error) {
+			captureException({
+				error,
+				showAlert: true,
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	}, [
+		params.petId,
+		pop,
+		administeredDate,
+		showUseDate,
+		nextDoseDate,
+		showUseNextDate,
+		name,
+		notes,
+	]);
+
+	const switchAdministeredDateModal = useCallback(() => {
+		setIsModalAdministeredDateVisible(prevValue => !prevValue);
+	}, []);
+
+	const switchNextDateModal = useCallback(() => {
+		setIsModalNextDateVisible(prevValue => !prevValue);
+	}, []);
+
+	return (
+		<Container>
+			<Header />
+
+			{isLoading ? (
+				<Loading />
+			) : (
+				<Content>
+					<Input
+						placeholder="Nome da vacina"
+						value={name}
+						onChangeText={setName}
+					/>
+
+					<DateContainer>
+						<DateTextContent onPress={switchAdministeredDateModal}>
+							<DateText>
+								{showUseDate
+									? formatDate(administeredDate)
+									: 'Adicionar data da aplicação'}
+							</DateText>
+						</DateTextContent>
+
+						{showUseDate && (
+							<DateRemoveButton
+								onPress={() => {
+									setAdministeredDate(new Date());
+									setShowUseDate(false);
+								}}
+							>
+								<DateRemoveIcon name="close-outline" />
+							</DateRemoveButton>
+						)}
+					</DateContainer>
+
+					<DateContainer>
+						<DateTextContent onPress={switchNextDateModal}>
+							<DateText>
+								{showUseNextDate
+									? formatDate(nextDoseDate)
+									: 'Adicionar proxima dose'}
+							</DateText>
+						</DateTextContent>
+
+						{showUseNextDate && (
+							<DateRemoveButton
+								onPress={() => {
+									setNextDoseDate(new Date());
+									setShowUseNextDate(false);
+								}}
+							>
+								<DateRemoveIcon name="close-outline" />
+							</DateRemoveButton>
+						)}
+					</DateContainer>
+
+					<Input
+						placeholder="Observações"
+						multiline
+						value={notes}
+						onChangeText={setNotes}
+					/>
+
+					<Button title="Adicionar vacina" onPress={handleSave} />
+				</Content>
+			)}
+
+			<Modal visible={isModalAdministeredDateVisible}>
+				<CloseButton onPress={switchAdministeredDateModal}>
+					<Icon name="close" />
+				</CloseButton>
+				<View
+					style={{
+						flex: 1,
+						justifyContent: 'center',
+					}}
+				>
+					<Label>Data da aplicação</Label>
+					<DatePicker
+						mode="single"
+						date={administeredDate}
+						onChange={change => {
+							if (change.date) {
+								setAdministeredDate(
+									new Date(String(change.date))
+								);
+
+								setShowUseDate(true);
+							}
+						}}
+					/>
+				</View>
+			</Modal>
+
+			<Modal visible={isModalNextDateVisible}>
+				<CloseButton onPress={switchNextDateModal}>
+					<Icon name="close" />
+				</CloseButton>
+				<View
+					style={{
+						flex: 1,
+						justifyContent: 'center',
+					}}
+				>
+					<Label>Data da próxima dose</Label>
+					<DatePicker
+						mode="single"
+						date={nextDoseDate}
+						onChange={change => {
+							if (change.date) {
+								setNextDoseDate(new Date(String(change.date)));
+
+								setShowUseNextDate(true);
+							}
+						}}
+					/>
+				</View>
+			</Modal>
+		</Container>
+	);
+};
+
+export default VaccinesAdd;
---
+++ src/views/vaccines/add/styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components/native';
+import { initialWindowMetrics } from 'react-native-safe-area-context';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
+export const Container = styled.ScrollView.attrs(() => ({
+	contentInsetAdjustmentBehavior: 'automatic',
+	alwaysBounceVertical: true,
+	contentContainerStyle: {
+		flexGrow: 1,
+	},
+}))``;
+
+export const Content = styled.View`
+	background-color: #ffffff;
+	border-radius: 12px;
+	margin: 0 20px;
+	padding: 15px;
+`;
+
+export const Input = styled.TextInput`
+	margin: 0 10px 10px;
+	border: 1px solid rgba(0, 0, 0, 0.1);
+	border-radius: 8px;
+	padding: 10px;
+	color: #000;
+`;
+
+export const DateContainer = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+
+	align-items: center;
+	margin: 5px 15px;
+`;
+
+export const DateTextContent = styled.TouchableOpacity``;
+
+export const DateText = styled.Text`
+	font-size: 14px;
+`;
+
+export const DateRemoveButton = styled.TouchableOpacity``;
+
+export const DateRemoveIcon = styled(Ionicons).attrs(() => ({
+	size: 23,
+	name: 'close-outline',
+}))``;
+
+export const Label = styled.Text`
+	font-size: 15px;
+	font-weight: bold;
+	color: #000000;
+	margin: 15px 10px;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+	margin-top: ${initialWindowMetrics?.insets.top}px;
+`;
+
+export const Icon = styled(Ionicons).attrs(() => ({
+	size: 35,
+}))`
+	align-self: flex-end;
+	margin: 0 10px;
+`;
