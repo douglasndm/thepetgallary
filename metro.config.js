7272c5ab4af09f0a31eef0bed25330d6d22f488f
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-09-23 12:40:34 -0300

    fix: lottie imports

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,19 @@
+const { mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const config = getSentryExpoConfig(__dirname);
+const defaultConfig = getSentryExpoConfig(__dirname);
 
-module.exports = config;
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+	resolver: {
+		assetExts: [...defaultConfig.resolver.assetExts, 'lottie'],
+	},
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
